1490d0e8885454396fd8f425fa1e9d1a004a9800
vivekprosperr <99174514+vivekprosperr@users.noreply.github.com>
2023-04-18 15:26:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const API_URL = 'https://randomuser.me/api';
+
+const App = () => {
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      const {title, first, last } = response.data.results[0].name;
+      const email = response.data.results[0].email;
+      const user = {
+        title: title,
+        firstName: first,
+        lastName: last,
+        email: email
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+      setUserData(user);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refreshData = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      const {title, first, last } = response.data.results[0].name;
+      const email = response.data.results[0].email;
+      const user = {
+        title: title,
+        firstName: first,
+        lastName: last,
+        email: email
+      };
+      localStorage.setItem('user', JSON.stringify(user));
+      setUserData(user);
+      setError('');
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {userData ? (
+        <>
+          <h1>{userData.title} {userData.firstName} {userData.lastName}</h1>
+          <p>{userData.email}</p>
+          <button onClick={refreshData}>Refresh</button>
+        </>
+      ) : (
+        <p>{error || 'Loading...'}</p>
+      )}
     </div>
   );
 }
