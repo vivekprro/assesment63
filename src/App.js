d83741ec98b2eb975a19c92abf4bd1555b514090
vivekprosperr <=>
2023-04-27 01:51:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import Loader from "./Loader";
 
-const API_URL = 'https://randomuser.me/api';
+const API_URL = "https://randomuser.me/api";
 
 const App = () => {
   const [userData, setUserData] = useState(null);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const fetchData = async () => {
     try {
       const response = await axios.get(API_URL);
-      const {title, first, last } = response.data.results[0].name;
+      const { title, first, last } = response.data.results[0].name;
       const email = response.data.results[0].email;
       const user = {
         title: title,
         firstName: first,
         lastName: last,
-        email: email
+        email: email,
       };
-      localStorage.setItem('user', JSON.stringify(user));
-      setUserData(user);
+      localStorage.setItem("user", JSON.stringify(user));
+      setTimeout(() => {
+        setUserData(user);
+      }, 1000);
     } catch (error) {
       setError(error.message);
     }
@@ -32,17 +36,20 @@ const App = () => {
   const refreshData = async () => {
     try {
       const response = await axios.get(API_URL);
-      const {title, first, last } = response.data.results[0].name;
+      const { title, first, last } = response.data.results[0].name;
       const email = response.data.results[0].email;
       const user = {
         title: title,
         firstName: first,
         lastName: last,
-        email: email
+        email: email,
       };
-      localStorage.setItem('user', JSON.stringify(user));
-      setUserData(user);
-      setError('');
+      localStorage.setItem("user", JSON.stringify(user));
+      setUserData(null);
+      setTimeout(() => {
+        setUserData(user);
+      }, 1000);
+      setError("");
     } catch (error) {
       setError(error.message);
     }
@@ -52,15 +59,17 @@ const App = () => {
     <div className="App">
       {userData ? (
         <>
-          <h1>{userData.title} {userData.firstName} {userData.lastName}</h1>
+          <h1>
+            {userData.title} {userData.firstName} {userData.lastName}
+          </h1>
           <p>{userData.email}</p>
           <button onClick={refreshData}>Refresh</button>
         </>
       ) : (
-        <p>{error || 'Loading...'}</p>
+        <p>{error || <Loader />}</p>
       )}
     </div>
   );
-}
+};
 
 export default App;
